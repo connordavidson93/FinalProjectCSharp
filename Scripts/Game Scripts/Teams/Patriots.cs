2e03d27fbc1fc8b982b81cdb6dbd96145ee5b2f8
Connor Davidson <connordavidson93@gmail.com>
2016-12-05 11:11:34 -0700

    Working with the lists and changing inheritance

--- Scripts/Game Scripts/Teams/Patriots.cs
+++ Scripts/Game Scripts/Teams/Patriots.cs
@@ -1,11 +1,12 @@
 //A team the player can choose to be
 using System.Collections.Generic;
 using System;
-public class Patriots: TeamBase{
+public class Patriots: Players{
     
 }
 
 /*Players are
-Tom Brady - Quarterback
-Julian Edelman - Receiver
-James White - Runningback
+Tom Brady #12- Quarterback
+Julian Edelman #11 - Receiver
+James White #28 - Runningback
+Chris Long #95 - Defense
---
+++ Scripts/Game Scripts/Teams/TeamMate.cs
@@ -0,0 +1,6 @@
+//Subjects for the list of players
+using System;
+public class TeamMate{
+    public string name;
+    public int number;
+}
